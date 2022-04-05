725b31b9a40e0a921bf68f51725ff985315332ee
walter rodrigues junior <walterrjr.86@gmail.com>
2022-04-05 10:22:42 -0300

    130

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -9,11 +9,11 @@
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
     <br>
 
-    <select name="motivo_contato" class="{{ $classe }}">
+    <select name="motivo_contatos_id" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
         {{-- neste caso, se 'motivo_contato' for igual ao value referido, mantem, se não, fica em branco --}}
         @foreach ($motivo_contatos as $key => $motivo_contato)
-            <option value="{{ $motivo_contato->id }}" {{ old('motivo_contato') == $motivo_contato->id ? 'selected' : '' }}>
+            <option value="{{ $motivo_contato->id }}" {{ old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : '' }}>
                 {{ $motivo_contato->motivo_contato }}</option>
         @endforeach
     </select>
