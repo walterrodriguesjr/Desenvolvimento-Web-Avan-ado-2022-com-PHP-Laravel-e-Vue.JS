b5e09d0553cc24f483dfa644617baf500bbc311c
walter rodrigues junior <walterrjr.86@gmail.com>
2022-04-06 18:24:29 -0300

    145

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -13,14 +13,29 @@ class AutenticacaoMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $metodo_autenticacao, $perfil, $param3, $param4)
     {
-        if(true){
+        //verifica se o usuário possui acesso a rota
+        echo $metodo_autenticacao.' - '.$perfil.'<br>';
+
+        if($metodo_autenticacao == 'padrao') {
+            echo 'Verificar o usuário e senha no banco de dados'.$perfil.'<br>';
+        }
+
+        if($metodo_autenticacao == 'ldap') {
+            echo 'Verificar o usuário e senha no AD'.$perfil.'<br>';
+        }
+
+        if($perfil == 'visitante') {
+            echo 'Exibir apenas alguns recursos';
+        } else {
+            echo 'Carregar o perfil no banco de dados';
+        }
+
+        if(false) {
             return $next($request);
-
-        }else{
-            return Response('Acesso negado! Rota exige autenticação!');
-
+        } else {
+            return Response('Acesso negado! Rota exige autenticação!!!');
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\LogAcessoMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,28 +11,31 @@ use App\Http\Middleware\LogAcessoMiddleware;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 /*
 Route::get('/', function () {
-    return 'Olá, seja bem vindo ao curso!';
+return 'Olá, seja bem vindo ao curso!';
 });
-*/
-       /* ROTAS COMUNS DO SITE */                                                      /* ATRIBUINDO O REFERIDO MIDDLEWARE A ESTA ROTA */
-Route::get('/', 'PrincipalController@principal')->name('site.index')/* ->middleware(LogAcessoMiddleware::class) */;
-Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos')/* ->middleware(LogAcessoMiddleware::class) */;
+ */
+/* ROTAS COMUNS DO SITE *//* ATRIBUINDO O REFERIDO MIDDLEWARE A ESTA ROTA */
+Route::get('/', 'PrincipalController@principal')->name('site.index') /* ->middleware(LogAcessoMiddleware::class) */;
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos') /* ->middleware(LogAcessoMiddleware::class) */;
 Route::get('/contato', 'ContatoController@contato')->name('site.contato')->middleware('log.acesso');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
-Route::get('/login', function(){return 'Login';})->name('site.login');
+/* ROTA DE LOGIN */
+Route::get('/login', 'LoginController@index')->name('site.login');
+Route::post('/login', 'LoginController@autenticar')->name('site.login');
 
 /* ROTAS ADMINISTRATIVAS DA APLICAÇÃO, SOMENTE PODEM SER ACESSADAS SE O USUÁRIO ESTIVER AUTENTICADO */
-Route::prefix('/app')->group(function() {                                                   /* INSERIDO UM ENCADEAMENTO DE 2 MIDDLEWARES */
-    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes')->middleware('autenticacao');
-    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores')->middleware('autenticacao');
-    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos')->middleware('autenticacao');
+/* MIDDLEWARE AUTENTICACAO PASSANDO O PARAMETRO 'PADRAO' */
+Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(function () {
+    Route::get('/clientes', function () {return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function () {return 'produtos';})->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
 
-Route::fallback(function() {
-    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="' . route('site.index') . '">clique aqui</a> para ir para página inicial';
 });
